781c96b0190265c186e939f71260ef44b906c221
Daniel Dushev <danieldyshew@gmail.com>
2023-11-05 13:24:23 +0200

    solved with better runtime

--- Top150/majority-emenet/majority-element.js
+++ Top150/majority-emenet/majority-element.js
@@ -2,23 +2,31 @@
  * @param {number[]} nums
  * @return {number}
  */
+// ~300ms runtime solution
+// var majorityElement = function(nums) {
+//     const half = nums.length / 2;
+//     nums.sort((a, b) => a - b);
+//     let counter = 1;
+//     let num1;
+//     let num2;
+//     while(counter <= half) {
+//         num1 = nums[0];
+//         num2 = nums[1];
+//         if(num1 == num2) {
+//             counter++
+//         }else{
+//             counter = 1;
+//         }
+//         nums.shift();
+//     }
+//     return nums[0];
+// };
+
+
+//~50ms runtime solution
 var majorityElement = function(nums) {
-    const half = nums.length / 2;
     nums.sort((a, b) => a - b);
-    let counter = 1;
-    let num1;
-    let num2;
-    while(counter <= half) {
-        num1 = nums[0];
-        num2 = nums[1];
-        if(num1 == num2) {
-            counter++
-        }else{
-            counter = 1;
-        }
-        nums.shift();
-    }
-    return nums[0];
+    return nums[Math.floor(nums.length / 2)];
 };
 
-console.log(majorityElement([3,2,3]));
+console.log(majorityElement([1]));
